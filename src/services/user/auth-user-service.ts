232beb28905369819a99d77a6e98659f478672e5
Alison Gabriel <odevalison@gmail.com>
2025-12-31 00:20:08 -0300

    feat(service): authenticate and generate user access token

--- src/services/user/auth-user-service.ts
+++ src/services/user/auth-user-service.ts
@@ -1,3 +1,8 @@
+import "dotenv/config";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import prismaClient from "../../prisma";
+
 interface AuthUserServiceProps {
   email: string;
   password: string;
@@ -5,7 +10,41 @@ interface AuthUserServiceProps {
 
 export class AuthUserService {
   async execute({ email, password }: AuthUserServiceProps) {
-    console.log({ email, password });
-    return "Logado com sucesso!";
+    const user = await prismaClient.user.findFirst({
+      where: { email },
+    });
+    const isUserNotExists = !user;
+
+    if (isUserNotExists) {
+      throw new Error("Invalid e-mail or password.");
+    }
+
+    const isPasswordMatch = await compare(password, user.password);
+    const isNotPasswordMatch = !isPasswordMatch;
+
+    if (isNotPasswordMatch) {
+      throw new Error("Incorrect e-mail or password.");
+    }
+
+    // generate JWT
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET!,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token,
+    };
   }
 }
